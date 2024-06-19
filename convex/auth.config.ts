42831ff8bc6387edbcf53b216c3b5e6d3fe3f732
Param Kamlesh Pathak <parampathak47@gmail.com>
2024-06-19 22:20:27 +0530

    Before clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://known-sawfly-2.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
---
+++ convex/http.ts
@@ -0,0 +1,2 @@
+//create a clerk user -> info -> users.create ->
+//create a podcast -> info -> podcasts.create ->
